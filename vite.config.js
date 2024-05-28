400e583022c99e8c7e7c5b0b7fd95a798403f0c5
mjcarbonell <maximcarbonell@gmail.com>
2024-05-28 14:59:46 -0700

    crafty change to config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // ensure the output directory is 'dist'
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    define: {
+      'process.env.VITE_OPENAI_KEY': JSON.stringify(env.VITE_OPENAI_KEY),
+    },
+    plugins: [react()],
+    build: {
+      outDir: 'dist', // ensure the output directory is 'dist'
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: 'assets/[name].[ext]'
+        }
+      }
+    },
+    server: {
+      mimeTypes: {
+        '.js': 'application/javascript'
       }
     }
-  },
-  server: {
-    mimeTypes: {
-      '.js': 'application/javascript'
-    }
-  }
+  };
 });
